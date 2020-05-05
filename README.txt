dee1848ac7fd342354816e15bba49b026028d885
Christophe Eymard <christophe.eymard@sales-way.com>
2020-05-05 23:00:54 +0200

    somewhat working

---
+++ README.txt
@@ -0,0 +1,5 @@
+A dockerfile to create an image of docker based on the official *alpine
+with pg_cron and psql-http.
+
+See 
+* https://github.com/pramsey/pgsql-http
--- root/install.sh
+++ root/install.sh
@@ -4,8 +4,8 @@ PG_CRON_VERSION="1.2.0"
 PGSQL_HTTP_VERSION="1.3.1"
 
 # Add base-build
-apk add libcurl
-apk add --virtual build-deps --no-cache build-base curl-dev clang llvm
+apk add libcurl readline bison perl zlib python3
+apk add --virtual build-deps --no-cache build-base curl-dev clang llvm postgresql-plpython3 postgresql-plpython3-contrib
 
 mkdir -p /build
 cd /build
@@ -23,6 +23,11 @@ cd pg_cron-$PG_CRON_VERSION
 # Complains for some reason
 sed -i 's/ -Werror//' Makefile
 make && make install
+
+
+cp -Rf /usr/share/postgresql/extension/*plpy* /usr/local/share/postgresql/extension/
+cp -Rf /usr/lib/postgresql/*plpy* /usr/local/lib/postgresql/
+cp -Rf /usr/lib/postgresql/pgxs/src/pl/plpython /usr/local/lib/postgresql/pgxs/src/pl/
 
 cd /
 rm -rf /build
